65332e292500ae0c87f19a470cd3249dc35f28ba
Nicolas Lujan <nico512lujan@gmail.com>
2025-08-19 18:23:47 -0300

    Added firebaseConfig and API URLS

---
+++ src/lambdaURL.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://lxo62uziutdiqh4ocqqicuv6qa0zyzvy.lambda-url.us-east-1.on.aws/"
+export const ENGINE_URL = "https://x7nr2tbuc5zbyhc56oaqolhpqa0zhvkt.lambda-url.us-east-1.on.aws/"
