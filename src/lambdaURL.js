eab869d579bc48e191f0e609dc7c8327a9ffaa73
Nicolas Lujan <nico512lujan@gmail.com>
2026-01-25 13:13:50 -0300

    Refactor: Configuración de variables de entorno y actualización de URLs Lambda

--- src/lambdaURL.js
+++ src/lambdaURL.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://lxo62uziutdiqh4ocqqicuv6qa0zyzvy.lambda-url.us-east-1.on.aws/"
-export const ENGINE_URL = "https://x7nr2tbuc5zbyhc56oaqolhpqa0zhvkt.lambda-url.us-east-1.on.aws/"
+export const API_URL = import.meta.env.VITE_AWS_API_URL;
+export const ENGINE_URL = import.meta.env.VITE_AWS_ENGINE_URL;
